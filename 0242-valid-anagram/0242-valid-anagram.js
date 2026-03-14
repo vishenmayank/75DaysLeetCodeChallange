6d2c208ae65b7647d663badb722c65679e64cb75
Mayank Kr. Singh <105173342+vishenmayank@users.noreply.github.com>
2026-03-14 22:35:42 +0530

    Time: 16 ms (44.88%), Space: 61.1 MB (12.31%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false;
+
+  const map = {};
+
+  for (let char of s) {
+    map[char] = (map[char] || 0) + 1;
+  }
+
+  for (let char of t) {
+    if (!map[char]) return false;
+    map[char]--;
+  }
+
+  return true;
+    
+};
